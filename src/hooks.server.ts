e8a1bafb6dfcf78c2e7998b9c08308d3fafdd615
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-11-21 00:05:08 +0300

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,8 +5,6 @@ import {createSupabaseServerClient} from '@supabase/auth-helpers-sveltekit'
 
 
 export const handle: Handle = async ({event , resolve}) => {
-   
- 
    event.locals.supabase = createSupabaseServerClient({
     supabaseKey:VITE_SUPABASE_ANON_KEY,
     supabaseUrl: VITE_SUPABASE_URL,
